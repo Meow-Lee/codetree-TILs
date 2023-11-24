309dff3534abcfbd3a7c6227f62d012f65edb601
Lee-MyungMo <67636286+Meow-Lee@users.noreply.github.com>
2023-11-24 19:28:09 +0900

    Title: 나이트, Time: 130ms, Memory: 9MB, Status: Passed - CodeTree

--- 231124/나이트/knight-movements.java
+++ 231124/나이트/knight-movements.java
@@ -20,17 +20,21 @@ public class Main {
 
         n = Integer.parseInt(br.readLine());
 
-        grid = new int[n + 1][n + 1];
-        visited = new boolean[n + 1][n + 1];
+        grid = new int[n][n];
+        visited = new boolean[n][n];
 
         StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
-        int x1 = Integer.parseInt(stk.nextToken());
-        int y1 = Integer.parseInt(stk.nextToken());
-        int x2 = Integer.parseInt(stk.nextToken());
-        int y2 = Integer.parseInt(stk.nextToken());
+        int x1 = Integer.parseInt(stk.nextToken()) - 1;
+        int y1 = Integer.parseInt(stk.nextToken()) - 1;
+        int x2 = Integer.parseInt(stk.nextToken()) - 1;
+        int y2 = Integer.parseInt(stk.nextToken()) - 1;
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(grid[i], -1);
+        }
 
         bfs(x1, y1, x2, y2);
-        if (grid[x2][y2] == 0) {
+        if (grid[x2][y2] == -1) {
             System.out.println(-1);
         } else {
             System.out.println(grid[x2][y2]);
@@ -40,6 +44,7 @@ public class Main {
     static void bfs(int x1, int y1, int x2, int y2) {
         Queue<Pair> q = new LinkedList<>();
         q.offer(new Pair(x1, y1));
+        grid[x1][y1] = 0;
         visited[x1][y1] = true;
 
         while (!q.isEmpty()) {
@@ -47,7 +52,7 @@ public class Main {
             for (int dir = 0; dir < 8; dir++) {
                 int nx = cur.x + dx[dir];
                 int ny = cur.y + dy[dir];
-                if (nx <= 0 || ny <= 0 || nx > n || ny > n) {
+                if (nx < 0 || ny < 0 || nx >= n || ny >= n) {
                     continue;
                 }
                 if (visited[nx][ny]) {
